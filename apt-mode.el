5a5d4259f411d4d1d0f1a9776c3ba71f27a86387
Josh Crean <jcrean@jcrean-lin2.ws.hmsonline.com>
2010-01-19 14:10:07 -0500

    set font-lock-defaults to enable some basic comment highlighting and allow for future customization of syntax highlighting

--- apt-mode.el
+++ apt-mode.el
@@ -15,11 +15,22 @@
     st)
   "Syntax table for 'apt-mode'.")
 
+;;
+;; Currently no font lock keywords, but can hook into this
+;; later to do some syntax highlighting. 
+;;
+;; Setting font-lock-defaults below enables comment highlighting
+;; (based on syntax-table defintion)
+;;
+(defvar apt-font-lock-keywords '())
+
 (define-derived-mode apt-mode fundamental-mode "APT"
   "A major mode for editing AlmostPlainText (.apt) files."
   :syntax-table apt-mode-syntax-table
-  (set (make-local-variable 'comment-start) "~~ ")
-  (set (make-local-variable 'comment-start-skip) "~~+\\s+"))
+  (set (make-local-variable 'comment-start) "~~")
+  (set (make-local-variable 'comment-start-skip) "~~+\\s*")
+  (set (make-local-variable 'font-lock-defaults) '(apt-font-lock-keywords))
+  )
 
 (defun insert-apt-header ()
   (interactive)
